bf79d2b3ffec008fc5651284c576a1076befba73
Yuan Liu <yuan6liu@gmail.com>
2019-12-16 15:44:34 -0500

    wip

--- cs231n/classifiers/linear_classifier.py
+++ cs231n/classifiers/linear_classifier.py
@@ -57,8 +57,10 @@ class LinearClassifier(object):
             #########################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-            pass
-
+            indices = np.random.choice(range(0,num_train), batch_size)
+            X_batch = X[indices,:]
+            y_batch = y[indices]
+            
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
             # evaluate loss and gradient
@@ -72,7 +74,7 @@ class LinearClassifier(object):
             #########################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-            pass
+            self.W += - learning_rate * grad
 
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
