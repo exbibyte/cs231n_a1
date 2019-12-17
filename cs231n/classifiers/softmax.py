a980fdc761b7ea2132f4ca56745987a6288ceb60
Yuan Liu <yuan6liu@gmail.com>
2019-12-17 00:48:00 -0500

    implemented: softmax

--- cs231n/classifiers/softmax.py
+++ cs231n/classifiers/softmax.py
@@ -33,8 +33,61 @@ def softmax_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    #-log(e^(y_i+c) / sum{j} e^(y_j+c)) = -(y_i+c) + log(sum{j} e^(y_j+c))
+    
+    num_data = X.shape[0]
+    num_feature = X.shape[1]
 
+    cache = np.dot(X,W)
+    
+    c = -np.amax(cache)
+    
+    dloss = 1
+
+    d_0 = dloss * 1/X.shape[0]
+    d_1 = -1.0 *d_0
+    
+    for i in xrange(num_data):
+
+        loss_temp = 0
+        denom =0
+        for k in xrange(W.shape[1]):
+            h = 0
+            for j in xrange(num_feature): #inner product
+                h += X[i,j] * W[j,k] #d_4
+            denom += np.exp(h+c) #d_3
+        
+        loss_temp = np.log(denom) #d_2
+
+        d_2 = d_0 * 1/denom
+        
+        #for computing derivatives
+        for k in xrange(W.shape[1]):
+            h = 0
+            for j in xrange(num_feature): #inner product
+                h += X[i,j] * W[j,k]
+            d_3 = d_2 * np.exp(h+c)
+            
+            for j in xrange(num_feature): #inner product
+                dW[j,k] += d_3 * X[i,j]
+            
+        loss_temp_2 = 0
+        for j in xrange(num_feature):
+            loss_temp_2 += (X[i,j] * W[j,y[i]])
+            dW[j,y[i]] += d_1 * X[i,j]
+            
+        loss_temp_2 += c
+
+        loss_temp_2 = -1.0 * loss_temp_2 #d_1
+
+        loss += loss_temp + loss_temp_2
+
+    loss *= 1/X.shape[0] #d_0
+
+    loss += reg * np.sum(W * W)
+
+    dW += 2.0 * reg * W
+    
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     return loss, dW
@@ -57,8 +110,31 @@ def softmax_loss_vectorized(W, X, y, reg):
     # regularization!                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    
+    cache = np.dot(X,W)
+        
+    c = -np.amax(cache)
 
-    pass
+    denom = np.sum(np.exp(np.dot(X,W) + c), axis=1) #d_0, d_1
+
+    loss_total = -(np.diag(np.dot(X,W)[:,y]) + c) + np.log(denom) #d_loss_total
+
+    loss = 1/X.shape[0] * np.sum(loss_total) + reg * np.sum(W * W)
+
+    #derivatives
+
+    dW += 2.0 * reg * W
+    
+    d_loss_total = 1/X.shape[0]
+
+    d_0 = np.divide(d_loss_total, denom)
+
+    d_1 = np.expand_dims(d_0,axis=1) * np.exp(np.dot(X,W) + c)
+    
+    dW += np.dot(X.T,d_1)
+
+    for i in xrange(X.shape[0]):
+        dW[:,y[i]] += d_loss_total * -1.0 * X[i,:]
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
